e0206bf5cb51075367c51b709a20a63366cbcded
kylesmeltzer <kyle_smeltzer@hotmail.com>
2019-05-16 13:26:25 -0400

    site upload

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', message: "Welcome to your first Express app" });
+  res.render('index', { title: 'Nintendo Gamecube', 
+  message: "this information page for one of Nintendo's best consoles.", overview: "The Nintendo GameCube is a home video game console released by Nintendo in Japan and North America in 2001 and Europe and Australia in 2002. The sixth-generation console is the successor to the Nintendo 64, designed to compete with Sony's PlayStation 2 and Microsoft's Xbox." });
 });
 
 module.exports = router;
